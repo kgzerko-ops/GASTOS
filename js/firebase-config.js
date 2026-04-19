8f4cd877e1a84c55b873496a9ef3784da78f9c6a
kgzerko-ops <kgzerko@gmail.com>
2026-04-19 16:15:25 +0200

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -19,7 +19,7 @@
 // ═══════════════════════════════════════════════════════════════
 
 export const firebaseConfig = {
-  apiKey: "AIzaSyAD5zUzaUuCo-ljLhnSu7W8DFU55aOEzzE",
+  apiKey: "AIzaSyDjQy5bZ6qI6_OPTk2_5efiHzwAFEI-48Q",
   authDomain: "gastos-ticket.firebaseapp.com",
   projectId: "gastos-ticket",
   storageBucket: "gastos-ticket.firebasestorage.app",
